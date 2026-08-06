b727052db6541969e9172fefafc7cc8ee3e71f18
saroj-nokia <sarojtaj77@gmail.com>
2026-08-06 06:48:30 +0545

    Revert "SystemUI: Implement SystemUIBoostFramework"
    This reverts commit 1873a50b4ea6ed6a45c9d5db6e7ecc2dfddab0b6.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -235,7 +235,6 @@ import com.android.systemui.statusbar.policy.KeyguardUserSwitcherView;
 import com.android.systemui.statusbar.policy.SplitShadeStateController;
 import com.android.systemui.unfold.SysUIUnfoldComponent;
 import com.android.systemui.util.Compile;
-import com.android.systemui.util.SystemUIBoostFramework;
 import com.android.systemui.util.Utils;
 import com.android.systemui.util.time.SystemClock;
 import com.android.wm.shell.animation.FlingAnimationUtils;
@@ -2188,7 +2187,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             @Override
             public void onAnimationCancel(Animator animation) {
                 mCancelled = true;
-                SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
             }
 
             @Override
@@ -2199,7 +2197,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     springBack();
                 } else {
                     onFlingEnd(mCancelled);
-                    SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
                 }
             }
         });
@@ -2208,7 +2205,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         }
         setAnimator(animator);
         animator.start();
-        SystemUIBoostFramework.getInstance().animationBoostOn(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
     }
 
     @VisibleForTesting
@@ -2927,7 +2923,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     private void onTrackingStarted() {
-        SystemUIBoostFramework.getInstance().animationBoostOn(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_TRACKING_NOTIFICATION_PANEL_VIEW);
         endClosing();
         mShadeRepository.setLegacyShadeTracking(true);
         if (mTrackingStartedListener != null) {
@@ -2957,7 +2952,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         // If we unlocked from a swipe, the user's finger might still be down after the
         // unlock animation ends. We need to wait until ACTION_UP to enable blurs again.
         mDepthController.setBlursDisabledForUnlock(false);
-        SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_TRACKING_NOTIFICATION_PANEL_VIEW);
     }
 
     private void updateMaxHeadsUpTranslation() {
@@ -3730,7 +3724,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mIsExpandingOrCollapsing = true;
             mQsController.onExpandingStarted(mQsController.getFullyExpanded());
         }
-        SystemUIBoostFramework.getInstance().animationBoostOn(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_SPEED_UP_QS_EXPANSION_ANIMATION);
     }
 
     void notifyExpandingFinished() {
@@ -3739,7 +3732,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mExpanding = false;
             onExpandingFinished();
         }
-        SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_SPEED_UP_QS_EXPANSION_ANIMATION);
     }
 
     float getTouchSlop(MotionEvent event) {
@@ -3790,10 +3782,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
      *                         gesture), we always play haptic.
      */
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
-        if (openingWithTouch) {
-            SystemUIBoostFramework.getInstance().animationBoostOn(
-                SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
-        }
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
             if (!openingWithTouch || !mHasVibratedOnOpen) {
                 performHapticFeedback(HapticFeedbackConstants.GESTURE_START);
@@ -3960,7 +3948,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private void springBack() {
         if (mOverExpansion == 0) {
             onFlingEnd(false /* cancelled */);
-            SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
             return;
         }
         mIsSpringBackAnimation = true;
@@ -3976,19 +3963,16 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             @Override
             public void onAnimationCancel(Animator animation) {
                 mCancelled = true;
-                SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
                 mIsSpringBackAnimation = false;
                 onFlingEnd(mCancelled);
-                SystemUIBoostFramework.getInstance().animationBoostOff(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
             }
         });
         setAnimator(animator);
         animator.start();
-        SystemUIBoostFramework.getInstance().animationBoostOn(SystemUIBoostFramework.REQUEST_ANIMATION_BOOST_TYPE_FLING_NOTIFICATION_PANEL_VIEW);
     }
 
     @VisibleForTesting
